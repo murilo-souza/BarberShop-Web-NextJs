772e55f2a09efc6cdd6adec88caa6f72e6f492fb
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-01-30 20:11:16 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
+import { format } from 'date-fns'
 import Header from '../_components/header'
+import { ptBR } from 'date-fns/locale'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Murilo</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   )
 }
