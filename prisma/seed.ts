1fa9ea9a3fe82cda94dacba6b5e74ba4ac97bdf2
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-01-31 17:40:00 -0300

    fix: seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -95,7 +95,8 @@ async function seedDatabase() {
         name: 'Hidratação',
         description: 'Hidratação profunda para cabelo e barba.',
         price: 25.0,
-        imageUrl: 'Fios hidratados, macios e brilhantes.',
+        imageUrl:
+          'https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png',
       },
     ]
 
