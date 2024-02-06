cd5c80a456f160f1ccf52b2c46c1f9db9dc3a7af
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-05 22:36:14 -0300

    fix: ensure first available day to book a service is tomorrow

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -17,7 +17,7 @@ import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { useEffect, useMemo, useState } from 'react'
 import { generateDayTimeList } from '../_helpers/hours'
-import { format, setHours, setMinutes } from 'date-fns'
+import { addDays, format, setHours, setMinutes } from 'date-fns'
 import { saveBooking } from '../_actions/save-booking'
 import { Loader2 } from 'lucide-react'
 import { toast } from 'sonner'
@@ -176,7 +176,7 @@ const ServiceItem = ({
                       selected={date}
                       onSelect={(date) => handleDateClick(date)}
                       locale={ptBR}
-                      fromDate={new Date()}
+                      fromDate={addDays(new Date(), 1)}
                       styles={{
                         head_cell: {
                           width: '100%',
