7281d3631250886d0ce0d5b8f2398e48cf28bf5e
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-09 18:17:51 -0300

    feat: responsive header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,21 +3,25 @@
 import Image from 'next/image'
 import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
-import { MenuIcon } from 'lucide-react'
+import { CalendarIcon, CircleUserRoundIcon, MenuIcon } from 'lucide-react'
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import SideMenu from './side-menu'
 import Link from 'next/link'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { useSession } from 'next-auth/react'
 
 const Header = () => {
+  const { data } = useSession()
+
   return (
     <header>
       <Card>
-        <CardContent className="p-5 justify-between items-center flex flex-row">
+        <CardContent className="p-5 md:px-32 justify-between items-center flex flex-row">
           <Link href="/">
             <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
           </Link>
           <Sheet>
-            <SheetTrigger asChild>
+            <SheetTrigger asChild className="md:hidden">
               <Button variant="outline" size="icon">
                 <MenuIcon size={18} />
               </Button>
@@ -27,6 +31,28 @@ const Header = () => {
               <SideMenu />
             </SheetContent>
           </Sheet>
+
+          <div className="hidden md:flex md:flex-row md:items-center gap-6">
+            <Button variant="ghost" asChild>
+              <Link href="/bookings">
+                <CalendarIcon size={18} className="md:mr-2" />
+                Agendamentos
+              </Link>
+            </Button>
+            {!data?.user ? (
+              <Button variant="default">
+                <CircleUserRoundIcon size={18} className="md:mr-2" />
+                Perfil
+              </Button>
+            ) : (
+              <Button variant="ghost" className="flex gap-2">
+                <Avatar className="md:w-8 md:h-8">
+                  <AvatarImage src={data.user.image ?? ''} />
+                </Avatar>
+                <p className="text-base font-bold">{data.user.name}</p>
+              </Button>
+            )}
+          </div>
         </CardContent>
       </Card>
     </header>
