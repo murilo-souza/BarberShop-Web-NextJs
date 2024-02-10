edc5f94741992e496fe456c80cbacd3931494564
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-09 22:05:36 -0300

    fix: elements size

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,24 +1,11 @@
 'use client'
 
-import BarbershopInfoItem from '@/app/_components/barbershop-info-item'
-import Header from '@/app/_components/header'
 import SideMenu from '@/app/_components/side-menu'
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from '@/app/_components/ui/avatar'
 import { Button } from '@/app/_components/ui/button'
 import { Card, CardContent } from '@/app/_components/ui/card'
 import { Sheet, SheetContent, SheetTrigger } from '@/app/_components/ui/sheet'
 import { Barbershop } from '@prisma/client'
-import {
-  ChevronLeftIcon,
-  MapPinIcon,
-  MenuIcon,
-  Smartphone,
-  StarIcon,
-} from 'lucide-react'
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
@@ -70,17 +57,15 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
         />
       </div>
 
-      <Header hiddenMobile="hidden md:block" />
-
-      <div className="md:flex md:flex-row md:items-start md:gap-10 md:mt-10">
+      <div className="md:mt-10">
         <div>
           <div className="hidden md:block md:pl-32">
             <Image
               src={barbershop.imageUrl}
-              width={758}
-              height={486}
+              width={920}
+              height={576}
               alt={barbershop.name}
-              style={{ objectFit: 'contain' }}
+              style={{ objectFit: 'cover' }}
               className="rounded-xl"
             />
           </div>
@@ -99,7 +84,7 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
               <StarIcon className="text-primary" size={18} />
               <p className="text-sm">5,0 (20 avaliações)</p>
             </div>
-            <Card>
+            <Card className="border-none">
               <CardContent className="hidden md:flex md:flex-col items-center justify-items-center md:w-[135px] md:h-16 md:rounded-lg md:pt-1">
                 <div className="flex items-center gap-1">
                   <StarIcon className="text-primary" size={18} />
@@ -110,45 +95,6 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
             </Card>
           </div>
         </div>
-        <BarbershopInfoItem barbershop={barbershop}>
-          <section className="pt-5 pb-5 border-b border-solid border-secondary">
-            <div className="flex flex-row items-center justify-between pb-[10px] ">
-              <span className="text-gray-400 text-sm">Segunda-Feira</span>
-              <p className="text-sm">Fechado</p>
-            </div>
-            <div className="flex flex-row items-center justify-between pb-[10px]">
-              <span className="text-gray-400 text-sm">Terça-Feira</span>
-              <p className="text-sm">09:00 - 21:00</p>
-            </div>
-            <div className="flex flex-row items-center justify-between pb-[10px]">
-              <span className="text-gray-400 text-sm">Quarta-Feira</span>
-              <p className="text-sm">09:00 - 21:00</p>
-            </div>
-            <div className="flex flex-row items-center justify-between pb-[10px]">
-              <span className="text-gray-400 text-sm">Quinta-Feira</span>
-              <p className="text-sm">09:00 - 21:00</p>
-            </div>
-            <div className="flex flex-row items-center justify-between pb-[10px]">
-              <span className="text-gray-400 text-sm">Sexta-Feira</span>
-              <p className="text-sm">09:00 - 21:00</p>
-            </div>
-            <div className="flex flex-row items-center justify-between pb-[10px]">
-              <span className="text-gray-400 text-sm">Sábado</span>
-              <p className="text-sm">08:00 - 21:00</p>
-            </div>
-            <div className="flex flex-row items-center justify-between">
-              <span className="text-gray-400 text-sm">Domingo</span>
-              <p className="text-sm">Fechado</p>
-            </div>
-          </section>
-
-          <section>
-            <div className="flex flex-row items-center justify-between pt-[41.5px]">
-              <span className="text-sm">Em parceria com</span>
-              <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
-            </div>
-          </section>
-        </BarbershopInfoItem>
       </div>
     </div>
   )
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -4,6 +4,9 @@ import ServiceItem from './_components/service-item'
 import { Service } from '@prisma/client'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/_lib/auth'
+import BarbershopInfoItem from '@/app/_components/barbershop-info-item'
+import Image from 'next/image'
+import Header from '@/app/_components/header'
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -35,20 +38,72 @@ const BarbershopDetailsPage = async ({
   }
 
   return (
-    <div>
-      <BarbershopInfo barbershop={barbershop} />
+    <>
+      <Header hiddenMobile="hidden md:block" />
 
-      <div className="px-5 flex flex-col gap-4 py-6">
-        {barbershop.services.map((service: Service) => (
-          <ServiceItem
-            key={service.id}
-            service={service}
-            isAuthenticated={!!session?.user}
-            barbershop={barbershop}
-          />
-        ))}
+      <div className="md:grid md:grid-cols-3 gap-10">
+        <div className="md:col-span-2">
+          <BarbershopInfo barbershop={barbershop} />
+
+          <div className="px-5 flex flex-col gap-4 py-6 md:pl-32 md:px-0 md:grid md:grid-cols-2">
+            {barbershop.services.map((service: Service) => (
+              <ServiceItem
+                key={service.id}
+                service={service}
+                isAuthenticated={!!session?.user}
+                barbershop={barbershop}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="md:w-96 mt-10 md:col-span-1">
+          <BarbershopInfoItem barbershop={barbershop}>
+            <section className="md:pt-5 md:pb-5 md:border-b md:border-solid md:border-secondary">
+              <div className="flex flex-row items-center justify-between pb-[10px] ">
+                <span className="text-gray-400 text-sm">Segunda-Feira</span>
+                <p className="text-sm">Fechado</p>
+              </div>
+              <div className="flex flex-row items-center justify-between pb-[10px]">
+                <span className="text-gray-400 text-sm">Terça-Feira</span>
+                <p className="text-sm">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row items-center justify-between pb-[10px]">
+                <span className="text-gray-400 text-sm">Quarta-Feira</span>
+                <p className="text-sm">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row items-center justify-between pb-[10px]">
+                <span className="text-gray-400 text-sm">Quinta-Feira</span>
+                <p className="text-sm">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row items-center justify-between pb-[10px]">
+                <span className="text-gray-400 text-sm">Sexta-Feira</span>
+                <p className="text-sm">09:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row items-center justify-between pb-[10px]">
+                <span className="text-gray-400 text-sm">Sábado</span>
+                <p className="text-sm">08:00 - 21:00</p>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <span className="text-gray-400 text-sm">Domingo</span>
+                <p className="text-sm">Fechado</p>
+              </div>
+            </section>
+
+            <section>
+              <div className="flex flex-row items-center justify-between pt-[41.5px]">
+                <span className="text-sm">Em parceria com</span>
+                <Image
+                  src="/logo.png"
+                  alt="FSW Barber"
+                  height={18}
+                  width={120}
+                />
+              </div>
+            </section>
+          </BarbershopInfoItem>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
