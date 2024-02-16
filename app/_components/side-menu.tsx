d301a055773af0059876df27920190800a434914
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-15 22:42:58 -0300

    feat: add alert dialog when user signOut

--- app/_components/side-menu.tsx
+++ app/_components/side-menu.tsx
@@ -14,7 +14,17 @@ import { Avatar, AvatarImage } from './ui/avatar'
 import Link from 'next/link'
 
 import { signOut, useSession } from 'next-auth/react'
-import { AlertDialog, AlertDialogTrigger } from './ui/alert-dialog'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from './ui/alert-dialog'
 import DialogSignIn from './dialog-signin'
 
 const SideMenu = () => {
@@ -39,10 +49,34 @@ const SideMenu = () => {
 
             <h2 className="font-bold capitalize">{data.user.name}</h2>
           </div>
-
-          <Button variant="secondary" size="icon">
-            <LogOutIcon onClick={handleLogoutClick} />
-          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="secondary" size="icon">
+                <LogOutIcon />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent className="w-[318px] rounded-lg">
+              <AlertDialogHeader>
+                <AlertDialogTitle className="text-base font-bold text-center">
+                  Sair
+                </AlertDialogTitle>
+                <AlertDialogDescription className="text-sm text-center">
+                  Deseja sair da plataforma?
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter className="flex-row gap-3">
+                <AlertDialogCancel className="w-full mt-0">
+                  Cancelar
+                </AlertDialogCancel>
+                <AlertDialogAction
+                  className="w-full"
+                  onClick={handleLogoutClick}
+                >
+                  Sair
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       ) : (
         <div className="flex flex-col px-5 py-6 gap-3">
